638d621b734882d160f63685ec533947dd89b91b
Miuxa Santos <miuxasantos06@gmail.com>
2025-03-26 11:49:42 -0300

    configuração do banco de dados

--- config/connect.php
+++ config/connect.php
@@ -1,1 +1,34 @@
 <!-- Conexão com o DB -->
+
+<?php 
+    
+    define('HOST', 'localhost');
+    define('DATABASE', 'crud-php-db');
+    define('USER', 'root');
+    define('PASSWORD', 'root');
+
+    class Connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+
+        }
+
+        function connectDatabase()
+        {
+            try
+            {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            }
+            catch (PDOException $e)
+            {
+                echo "Error!".$e->getMessage();
+                die();
+            }
+        }
+    }
+
+    $testConnection = new Connect();
+?>
